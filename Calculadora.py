5d3ab1c6067f8f9b0de5a71e3ca4854169efb16b
Roberto Donatti Brandini <122102844+RobertoDonatti@users.noreply.github.com>
2023-09-20 17:18:33 -0400

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,24 @@
+n1 = 0
+n2 = 0
+operacao = ''
+resultado = 0
+
+n1 = int(input("digite o primeiro numero "))
+operacao = input("digite a operação desejada ")
+n2 = int(input("digite o segundo numero "))
+
+if operacao == '+':
+    resultado = n1 + n2
+    print(resultado)
+elif operacao == '-':
+    resultado = n1 - n2
+    print(resultado)
+elif operacao == '/':
+    resultado = n1/n2
+    print(resultado)
+elif operacao == '*':
+    resultado = n1 * n2
+    print(resultado)
+else:
+    resultado = 'n da n gay'
+
